754a0285dda2ffd70a73af6d4b817c156fc05fc3
LEX1005001 <AleX69.galkin@yandex.ru>
2024-04-07 17:14:24 +0300

    DataBase

--- DictionaryDataSourceTestConsoleApp/Program.cs
+++ DictionaryDataSourceTestConsoleApp/Program.cs
@@ -5,35 +5,29 @@ using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DictionatyDataSource;
+
 
 namespace DictionaryDataSourceTestConsoleApp
 {
     internal class Program
     {
-        static SQLiteConnection connection;
-        static SQLiteCommand command;
-
+       /* static SQLiteConnection connection;
+        static SQLiteCommand command;*/
+        public DataBase dataBase;
         static void Main(string[] args)
         {
             try
             {
-                connection = new SQLiteConnection("Data Source = D:\\DataBase\\DictionaryDB.db;Version=3; FailIfMissing=False");
-                connection.Open();
+                DataBase dataBase= new DataBase();
+                dataBase.Connection.Open();
                 Console.WriteLine("Connected!");
-                command = new SQLiteCommand(connection)
-                {
-                    CommandText = "SELECT * FROM \"WordDictionary\";"
-                };
-                Console.WriteLine("Результат запроса:");
-                DataTable data = new DataTable();
-                SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
-                adapter.Fill(data);
-                Console.WriteLine($"Прочитано {data.Rows.Count} записей из таблицы БД");
-                foreach (DataRow row in data.Rows)
-                {
-                    Console.WriteLine($"id = {row.Field<Int64>("Id")} themeId = {row.Field<Int64>("ThemeId")} wordId = {row.Field<Int64>("WordId")} translation = {row.Field<string>("Translation")}");
-                }
-
+                dataBase.GetAll();
+                Console.WriteLine();
+                dataBase.GetAll_themes();
+                Console.WriteLine();
+                dataBase.GetAll_words();
+                
             }
             catch (SQLiteException ex)
             {
--- DictionatyDataSource/DataSource.cs
+++ DictionatyDataSource/DataSource.cs
@@ -1,12 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DictionatyDataSource
 {
-    public class Class1
+    public class DataBase
     {
+        private SQLiteConnection connection = new SQLiteConnection("Data Source = D:\\DataBase\\DictionaryDB.db;Version=3; FailIfMissing=False");
+        private SQLiteCommand command;
+        
+
+       
+        public SQLiteCommand Command { get => command; set => command = value; }
+        public SQLiteConnection Connection { get => connection; set => connection = value; }
+
+        public void GetAll()
+        {
+            command = new SQLiteCommand(connection)
+            {
+                CommandText = "SELECT * FROM \"WordDictionary\";"
+            };
+            Console.WriteLine("Результат запроса GetAll:");
+            DataTable data = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+            adapter.Fill(data);
+            Console.WriteLine($"Прочитано {data.Rows.Count} записей из таблицы БД");
+            foreach (DataRow row in data.Rows)
+            {
+               Console.WriteLine($"id = {row.Field<Int64>("Id")} themeId = {row.Field<Int64>("ThemeId")} wordId = {row.Field<Int64>("WordId")} translation = {row.Field<string>("Translation")}");
+            }
+        }
+        public void GetAll_themes()
+        {
+            command = new SQLiteCommand(connection)
+            {
+                CommandText = "SELECT * FROM \"Theme\";"
+            };
+            Console.WriteLine("Результат запроса GetAll_themes:");
+            DataTable data = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+            adapter.Fill(data);
+            Console.WriteLine($"Прочитано {data.Rows.Count} записей из таблицы БД");
+            foreach(DataRow row in data.Rows)
+            {
+                Console.WriteLine($"id = {row.Field<Int64>("Id")} name = {row.Field<string>("Name")}");
+            }
+        }
+        public void GetAll_words()
+        {
+            command = new SQLiteCommand(connection)
+            {
+                CommandText = "SELECT * FROM \"Word\";"
+            };
+            Console.WriteLine("Результат запроса GetAll_words:");
+            DataTable data = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+            adapter.Fill(data);
+            Console.WriteLine($"Прочитано {data.Rows.Count} записей из таблицы БД");
+            foreach (DataRow row in data.Rows)
+            {
+                Console.WriteLine($"id = {row.Field<Int64>("Id")} this_word = {row.Field<string>("thisWord")}");
+            }
+        }
     }
+
 }
