88690cb7a0c1b1f36a8e5890629103dd23f34681
LEX1005001 <AleX69.galkin@yandex.ru>
2024-04-29 02:05:13 +0300

    UWU

--- DictionaryUI_WPF/ViewModel/HomeViewModel.cs
+++ DictionaryUI_WPF/ViewModel/HomeViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DictionaryUI_WPF.Model;
 
 namespace DictionaryUI_WPF.ViewModel
 {
-    internal class HomeViewModel
+    class HomeViewModel:Utilites.ViewModelBase
     {
+        private readonly PageModel _pageModel;
     }
 }
